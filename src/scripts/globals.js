650d9da63e78dfbaffb164369360b697341fb855
poppie <hoolm-2012@yandex.ru>
2023-12-15 23:51:14 +0500

    base UI

--- src/scripts/globals.js
+++
@@ -1,8 +0,0 @@
-export const gameObject = [];
-
-export const RECT_SIZE = 16;
-
-export const GAME_FIELD_WIDTH = Math.floor(window.screen.availWidth / RECT_SIZE - 1) * RECT_SIZE;
-export const GAME_FIELD_HEIGHT = Math.floor(window.screen.availHeight / RECT_SIZE - 6) * RECT_SIZE;
-
-export const FPS = (fps) => 100 / fps;
